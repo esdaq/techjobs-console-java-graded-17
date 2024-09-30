d9663ce092ec97606e6c54acb4bd1f800ae13776
d-esda <onedesda@gmail.com>
2024-09-30 19:52:18 -0400

    github action test

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,6 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
         if(!someJobs.isEmpty()) {
             for(HashMap<String, String> jobMap: someJobs){
                 System.out.println();
